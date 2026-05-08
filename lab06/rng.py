f05ef26adecbafb5664649308287bfbbb357c848
louisquincy <89979832+louisquincy@users.noreply.github.com>
2026-05-08 11:53:26 +0700

    Update rng.py

--- lab06/rng.py
+++ lab06/rng.py
@@ -1,7 +1,6 @@
 import time
 
 class MultiKongGen:
-    """Собственный ГПСЧ на основе линейного конгруэнтного метода."""
     def __init__(self, seed=None):
         self.M = 2 ** 63
         self.beta = 2 ** 32 + 3
@@ -10,6 +9,5 @@ class MultiKongGen:
         self.x0 = seed
 
     def anabios(self):
-        """Возвращает случайное число в [0, 1)."""
         self.x0 = (self.beta * self.x0) % self.M
         return self.x0 / self.M
